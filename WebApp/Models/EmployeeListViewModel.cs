f8e11e882771d2481ab8c1d4fa48b0e731abc0ab
songtaojie <songtaojie@aio5.com>
2018-07-06 17:52:21 +0800

    添加MVC实例

---
+++ WebApp/Models/EmployeeListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class EmployeeListViewModel
+    {
+        public string UsingName { get; set; }
+        public FooterViewModel FooterData { get; set; }
+    }
+}
